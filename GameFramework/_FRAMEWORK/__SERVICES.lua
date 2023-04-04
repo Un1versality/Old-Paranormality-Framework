ff7464128c7921951da7cdfc25ad5d3cf2a26fa1
Un1versality_Dev <117466324+Un1versality@users.noreply.github.com>
2023-04-04 09:31:40 -0700

    Create __SERVICES.lua

---
+++ GameFramework/_FRAMEWORK/__SERVICES.lua
@@ -0,0 +1,12 @@
+local GuiService = game:GetService("GuiService")
+local ReplicatedFirst = game:GetService("ReplicatedFirst")
+local UserInputService = game:GetService("UserInputService")
+return {
+    Players = game:GetService("Players");
+    ReplicatedStorage = game:GetService("ReplicatedStorage");
+    ServerStorage = game:GetService("ServerStorage");
+    ReplicatedFirst = game:GetService("ReplicatedFirst");
+    StarterGui = game:GetService("StarterGui");
+    GuiService = game:GetService("GuiService");
+    UserInputService = game:GetService("UserInputService");
+}
